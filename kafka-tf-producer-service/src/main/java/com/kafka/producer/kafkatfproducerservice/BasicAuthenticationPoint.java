d776203fc6ca83e5f551e121ce485c5a4b7b3707
jeyasekar <50139516+jeyasekar@users.noreply.github.com>
2019-06-14 21:21:29 +0530

    Add files via upload

---
+++ kafka-tf-producer-service/src/main/java/com/kafka/producer/kafkatfproducerservice/BasicAuthenticationPoint.java
@@ -0,0 +1,30 @@
+package com.kafka.producer.kafkatfproducerservice;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BasicAuthenticationPoint extends BasicAuthenticationEntryPoint {
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException, ServletException {
+		response.addHeader("WWW-Authenticate", "Basic realm=" + getRealmName());
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		PrintWriter writer = response.getWriter();
+		writer.println("HTTP Status 401 - " + authException.getMessage());
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		setRealmName("study");
+		super.afterPropertiesSet();
+	}
+}
